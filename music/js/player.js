867a6b60245141c29e5b0fc8c839edf78e8f827d
liubingyang1 <liubingyang1@qq.com>
2017-03-28 22:29:51 +0800

    跨域2

--- music/js/player.js
+++ music/js/player.js
@@ -1,11 +1,9 @@
 function play(data){
 	$.ajax({
 			type:"get",
-			url:"http://musicapi.duapp.com/api.php?type=url&id="+data.id,
-			async:false,
+			url:"http://musicapi.duapp.com/api.php?type=url&id="+data.id,			
 			dataType: 'jsonp',
-			data: qsData,
-			timeout: 5000,
+			jsonp:"jsonpcallback",
 			success: function(data){
 				console.log(data)
 				$('#myvideo')[0].src=data.data[0].url;
