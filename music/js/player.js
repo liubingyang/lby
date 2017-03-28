374be54a4c880a1cc98aadd6555a18b748b1ed10
liubingyang <252700620@qq.com>
2017-03-28 19:01:41 +0800

    自己的音乐

---
+++ music/js/player.js
@@ -0,0 +1,12 @@
+function play(data){
+	$.ajax({
+			type:"get",
+			url:"http://musicapi.duapp.com/api.php?type=url&id="+data.id,
+			async:true,
+			success: function(data){
+				console.log(data)
+				$('#myvideo')[0].src=data.data[0].url;
+				$('#myvideo')[0].play();				
+			}
+	});	
+}
